a0b1775eec3596ecdf6109391b6cd0de0f2b1f60
grushman@gmail.com <grushman@gmail.com>
2018-08-28 23:47:12 +0200

    Added Maven support

---
+++ src/main/java/com/itcourses/summer/shared/Main.java
@@ -0,0 +1,22 @@
+package com.itcourses.summer.shared;
+
+public class Main {
+
+    public static void main(String[] args) {
+      args = new String[]{"add","23","13"};
+	  if (args.length!=3){
+	      System.out.println("Wrong number of parameters");
+      }
+      int firstNumber = Integer.parseInt(args[1]);
+      int secondNumber = Integer.parseInt(args[2]);
+      switch (args[0].toLowerCase()){
+          case "add" : break;
+          case "divide" : break;
+          case "multiply" : break;
+          case "subtract" : break;
+          default:
+              System.out.println("Unknown command");
+        }
+
+    }
+}
