6cbb2190d5cb87ebde5f5453f7b9a4e87cafe193
mickru <42805113+mickru@users.noreply.github.com>
2018-08-30 18:57:09 +0200

    add test comment

--- src/main/java/com/itcourses/summer/shared/Main.java
+++ src/main/java/com/itcourses/summer/shared/Main.java
@@ -30,7 +30,7 @@ public class Main {
 
     }
 
-//new comment
+//new comment + smth
     public static int subtract(int firstNumber, int secondNumber) {
         return (firstNumber-secondNumber); }
 
